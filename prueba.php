f360caa553b051a1c5c0890f724624427fda716b
CarmenMAP <cmap070589@gmail.com>
2017-11-16 16:47:11 +0100

    cambiosCarmen

--- prueba.php
+++ prueba.php
@@ -1,1 +1,12 @@
+<?php
+
+.EstiloTexto{
+    color:#FE0000;
+    font-weight: bold;
+    text-align: center;
+    font-family:Verdana, Arial, Helvetica, sans-serif;
+    font-size: 25px;
+}
+
 echo "Hello Yoli";
+echo "<span class='EstiloTexto'>Hola a todos</span>";
